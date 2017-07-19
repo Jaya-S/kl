e8a613ff36234203baa9e7283a964d3279a06ab4
Jaya-S <jaivedgiri@gmail.com>
2017-07-19 17:58:14 +0530

    Create oddnumber.java

---
+++ oddnumber.java
@@ -0,0 +1,18 @@
+# kl
+import java. util.Scanner;
+public class oddnumber{
+ 	public static void main(String []args){
+  		Scanner s=new Scanner(System.in);
+  		int i;
+    	   System.out.println("Enter the numbers");
+  		i=s.nextInt();
+    		for(int i1=1;i1<10;i1++){
+   			if(i1%2!=0){
+        				System.out.println("i1");
+   			}
+   			
+  		}
+  		
+  	}
+ 	
+}
